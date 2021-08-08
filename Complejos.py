c46f6ba53bce06a566029c9d12bccc96e1333a5e
santiago <spereira98@misena.edu.co>
2021-08-08 16:55:37 -0500

    Desarrollo punto 2
    Este es el desarrollo del segundo punto

---
+++ Complejos.py
@@ -0,0 +1,13 @@
+
+class Complejo:
+    A = (6.3)
+    B = (7.2)
+    Suma = A + complex(B)
+    Resta = B - complex(A)
+    Multiplicar = A * complex(B)
+    Dividir = B / complex(A)
+    print(Suma)
+    print(Resta)
+    print(Multiplicar)
+    print(Dividir)
+    
